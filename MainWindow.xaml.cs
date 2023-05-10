61f593ca55646bdfcf75658f0b07652b69fdb2d5
Hansi Xu <hxacs7755@gmail.com>
2023-05-10 10:03:30 -0400

    Implemented New, Open, Save, Save As for service lists

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -7,6 +7,8 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Xml.Serialization;
+using Microsoft.Win32;
+using System.Collections.Specialized;
 
 namespace PortieTalkie
 {
@@ -18,11 +20,14 @@ namespace PortieTalkie
         ObservableCollection<Service> services = new ObservableCollection<Service>(){};
         ObservableCollection<string> comboBoxInputs = new ObservableCollection<string>();
         List<TalkieWindow> talkieWindows = new List<TalkieWindow>();
+        const string servicesXmlName = "Services.xml";
+        string currentXmlFile = servicesXmlName;
+        bool newUntitledFile = false;      // for Save behavior
+        bool madeChanges = false;          // ditto
+        
         public MainWindow()
         {
             InitializeComponent();
-
-            const string servicesXmlName = "Services.xml";
             Loaded += (sender, e) =>
             {  // load the services from last time
                 XmlSerializer serializer = new XmlSerializer(typeof(ObservableCollection<Service>));
@@ -30,7 +35,8 @@ namespace PortieTalkie
             };
             Closing += (sender, e) =>
             {  // save the services
-                if (MessageBoxResult.OK == MessageBox.Show("Quit PortieTalkie?", "", MessageBoxButton.OKCancel, MessageBoxImage.Question))
+                if (MessageBoxResult.OK == MessageBox.Show("Quit PortieTalkie? The current services will be saved to the default list for your next launch.", 
+                    "", MessageBoxButton.OKCancel, MessageBoxImage.Question))
                 {
                     WriteServices(servicesXmlName);
                     foreach (var talkieWindow in talkieWindows)
@@ -46,6 +52,41 @@ namespace PortieTalkie
             cbHostPort.ItemsSource = comboBoxInputs;
         }
 
+        private void Services_CollectionChanged(object? sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            switch (e.Action)
+            {
+                case NotifyCollectionChangedAction.Add:
+                case NotifyCollectionChangedAction.Remove:
+                case NotifyCollectionChangedAction.Replace:
+                case NotifyCollectionChangedAction.Move:
+                    madeChanges = true;
+                    updateTitle();
+                    break;
+                case NotifyCollectionChangedAction.Reset:
+                    // User got a new file, don't need to do anything (for now)
+                    break;
+                default:
+                    break;
+            }
+        }
+        ////////////////////////////////// Helpers //////////////////////////////////
+        private void updateTitle()
+        {
+            if (newUntitledFile)
+            {
+                Title = "PortieTalkie : UntitledList" + (madeChanges ? " *" : "");
+            } 
+            else
+            {
+                string xmlFileName = Path.GetFileName(currentXmlFile);
+                Title = "PortieTalkie : " + xmlFileName + (madeChanges ? " *" : "");
+                if (xmlFileName == "Services.xml")
+                {
+                    Title += "[Default list]";
+                }
+            }
+        }
 
         ////////////////////////////////// ListView events //////////////////////////////////
         private void listView_MouseDoubleClick(object sender, MouseButtonEventArgs e)
@@ -109,19 +150,76 @@ namespace PortieTalkie
             Close();    // Do not use Environment.Exit() so that the Closing event can trigger
         }
         ////////////////////////////////// Application Commands //////////////////////////////////
+
+        private void New_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (madeChanges)
+            {
+                var result = MessageBox.Show("You will any changes you've made since your last save. Continue?",
+                    "Warning", MessageBoxButton.OKCancel, MessageBoxImage.Exclamation);
+                if (result == MessageBoxResult.Cancel)
+                {
+                    return;
+                }
+            }
+            services.Clear();
+            newUntitledFile = true;
+            updateTitle();
+        }
         private void Open_Executed(object sender, ExecutedRoutedEventArgs e)
         {
-            MessageBox.Show("Open");
+            if (madeChanges)
+            {
+                var result = MessageBox.Show("You will any changes you've made since your last save. Continue?",
+                    "Warning", MessageBoxButton.OKCancel, MessageBoxImage.Exclamation);
+                if (result == MessageBoxResult.Cancel) 
+                {
+                    return;
+                }
+            }
+            var openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "XML files (*.xml)|*.xml|All files (*.*)|*.*";
+            openFileDialog.FilterIndex = 1;
+            openFileDialog.Multiselect = false;
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                currentXmlFile = openFileDialog.FileName;
+                ReadServices(currentXmlFile);
+                updateTitle();
+                // no need to toggle asterisk because the title is fresh
+            }
+
         }
 
         private void Save_Executed(object sender, ExecutedRoutedEventArgs e)
         {
-            MessageBox.Show("Save");
+            if (!newUntitledFile)
+            {
+                WriteServices(currentXmlFile);
+                madeChanges = false;
+                updateTitle();
+            } 
+            else
+            {
+                SaveAs_Executed(sender, e);
+            }
         }
 
         private void SaveAs_Executed(object sender, ExecutedRoutedEventArgs e)
         {
-            MessageBox.Show("SaveAs");
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "XML files (*.xml)|*.xml|All files (*.*)|*.*";
+            saveFileDialog.FilterIndex = 1;
+            
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                currentXmlFile = saveFileDialog.FileName;
+                WriteServices(currentXmlFile);
+                newUntitledFile = false;
+                madeChanges = false;
+                updateTitle();
+            }
         }
 
         ////////////////////////////////// Button click events //////////////////////////////////
@@ -149,7 +247,7 @@ namespace PortieTalkie
             {
                 MessageBox.Show(ae.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
             }
-
+            cbHostPort.Text = string.Empty;
         }
         ////////////////////////////////// ComboBox events //////////////////////////////////
         private void cbHostPort_KeyDown(object sender, KeyEventArgs e)
@@ -171,6 +269,7 @@ namespace PortieTalkie
                     if (servicesLoaded != null)
                     {
                         services = servicesLoaded;
+                        services.CollectionChanged += Services_CollectionChanged;
                         Dispatcher.Invoke(() =>
                         {
                             listView.DataContext = services;
